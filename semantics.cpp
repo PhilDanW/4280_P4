979fabdb8d0e041c4749ae32667164d3109bbedc
PhilDanW <42147737+PhilDanW@users.noreply.github.com>
2021-05-10 20:07:12 -0500

    Update semantics.cpp

--- semantics.cpp
+++ semantics.cpp
@@ -10,20 +10,22 @@ bool debugger2 = true;
 
 int var_count = 0, scope = 0;  //initialize the var count and the level of scope
 
-static int temp_var_count = 0;
+static int var_count_t = 0;
 /* total number of labels */
 static int label_count = 0;
 /* array to hold temp variables */
 std::string temp_vars[MAX_STACK];
-unsigned int current_temp_vars_num = 0;
-unsigned int carry_labels_num = 0;
-unsigned int current_labels_num = 0;
+unsigned int t_var_index = 0;
+unsigned int loop_index = 0;
 
-vector<var_hold> VARIABLE;
+vector<var_hold> VARIABLE(0);
 
-void codeGen(Node* node, int index, std::string output_file_name) {
+int var_ = 0;
+int var_value = 0;
 
-    file.open("file.asm", std::ios_base::trunc);
+void codeGen(treeNode* node, int index, std::string output_file_name) {
+
+    file.open(output_file_name, std::ios_base::trunc);
     semantic_check(node, index);  //checks semantics
     file.close();
 }
@@ -44,7 +46,7 @@ void push(Token tk) {
         }
 
         stack[var_count] = tk; if (debugger2) { std::cout << "Adding \'" << tk.token_string << "\' to the stack\n"; }  // for debugging
-        //file << "\tPUSH\n";
+        file << "PUSH" << std::endl;
         var_count++; if (debugger2) { std::cout << "VAR COUNT: " << var_count << "\n"; }  // for debugging
         //show_stack();
     }
@@ -83,215 +85,263 @@ bool var_exists(Token tk)
 }
 
 // goes through parsetree to check semantics
-void semantic_check(Node* node, int index)
+void semantic_check(treeNode* node, int index)
 {
     
     if (node == nullptr)
         return;
     if (debug1) {
-        if (node->name == "<vars>" || node->name == "<assign>") { std::cout << "Working on: " << std::setw(10) << std::left << node->name << "| Token: " << std::setw(9) << std::left << node->token_2.token_string << "| "; }  // for debugging
-        else { std::cout << "Working on: " << std::setw(10) << std::left << node->name << "| Token: " << std::setw(9) << std::left << node->token_1.token_string << "| "; }  // for debugging
+        if (node->label == "<vars>" || node->label == "<assign>") { std::cout << "Working on: " << std::setw(10) << std::left << node->label << "| Token: " << std::setw(9) << std::left << node->token2.token_string << "| "; }  // for debugging
+        else { std::cout << "Working on: " << std::setw(10) << std::left << node->label << "| Token: " << std::setw(9) << std::left << node->token1.token_string << "| "; }  // for debugging
         show_stack();
     }
 
 
-    if (node->name == "<program>")
+    if (node->label == "<program>")
     {
         int vars = 0;
-        check_children(node, vars);
+        if (node->leaf1 != nullptr)
+            semantic_check(node->leaf1, index);
+        if (node->leaf2 != nullptr)
+            semantic_check(node->leaf2, index);
+        if (node->leaf3 != nullptr)
+            semantic_check(node->leaf3, index);
+        if (node->leaf4 != nullptr)
+            semantic_check(node->leaf4, index);
         //Program stopping point
         file << "STOP" << std::endl;
-
 
         for (unsigned int i = 0; i < VARIABLE.size(); i++) {
             file << VARIABLE[i].ID << " " << VARIABLE[i].value << std::endl;
         }
 
-        for (int i = 0; i < MAX_STACK; i++)
+        for (int i = 0; i < t_var_index; i++)
         {
-            if (temp_vars[i] != "")
-                file << temp_vars[i] << " 0\n";
+            file << "T" << i << " 0\n";
         }
-
-
     }
 
 
-    else if (node->name == "<vars>")
+    else if (node->label == "<vars>")
     {
-        int tos_distance = topOfStackD(node->token_2);
+        int tos_distance = topOfStackD(node->token2);
         scope = var_count;
 
         if (tos_distance == -1 || tos_distance > index){
-
-            push(node->token_2);
+            var_hold tempVar;
+            tempVar.ID = node->token2.token_string;
+            tempVar.value = node->token4.token_string;
+            VARIABLE.resize(++var_, tempVar);
+            push(node->token2);
             index++;
         }
         else if (tos_distance < index){
-            error_declared(node->token_2.token_string);
+            error_declared(node->token2.token_string);
             exit(EXIT_FAILURE);
         }
 
-        if (node->child_1 != nullptr)
-            semantic_check(node->child_1, index);
+        if (node->leaf1 != nullptr)
+            semantic_check(node->leaf1, index);
     }
 
 
-    else if (node->name == "<block>"){
+    else if (node->label == "<block>"){
         unsigned int vars = 0;
         scope = var_count;
 
-        check_children(node, vars);
+        if (node->leaf1 != nullptr)
+            semantic_check(node->leaf1, index);
+        if (node->leaf2 != nullptr)
+            semantic_check(node->leaf2, index);
+        if (node->leaf3 != nullptr)
+            semantic_check(node->leaf3, index);
+        if (node->leaf4 != nullptr)
+            semantic_check(node->leaf4, index);
         pop(scope);
     }
 
 
-    else if (node->name == "<expr>"){
-        if (node->token_1.token_ID == MINUS_TK){
-            int vars_num = current_temp_vars_num++;
+    else if (node->label == "<expr>"){
+        if (node->token1.tokenID == SUBTRACT_OPERATOR){
+            int vars_num = t_var_index++;
             file << "SUB T" << vars_num << std::endl;
-            check_children(node, index);
+            if (node->leaf1 != nullptr)
+                semantic_check(node->leaf1, index);
+            if (node->leaf2 != nullptr)
+                semantic_check(node->leaf2, index);
+            if (node->leaf3 != nullptr)
+                semantic_check(node->leaf3, index);
+            if (node->leaf4 != nullptr)
+                semantic_check(node->leaf4, index);
         }
-        else if (node->child_1 != nullptr)
-            semantic_check(node->child_1, index);
+        else if (node->leaf1 != nullptr)
+            semantic_check(node->leaf1, index);
     }
 
 
-    else if (node->name == "<N>"){
-        int vars_num = current_temp_vars_num++;
+    else if (node->label == "<N>"){
+        int vars_num = t_var_index++;
         file << "STORE T" << vars_num << std::endl;
-        if (node->token_1.token_ID == SLASH_TK || node->token_1.token_ID == ASTERISK_TK){
-            
-            
-            
-            if (node->token_1.token_ID == SLASH_TK) {
+        if (node->token1.tokenID == DIVIDE_OPERATOR || node->token1.tokenID == ASTERISK_OPERATOR){
+
+            if (node->token1.tokenID == DIVIDE_OPERATOR) {
                 file << "DIV T" << vars_num << std::endl;
             }
-            else if (node->token_1.token_ID == ASTERISK_TK) {
+            else if (node->token1.tokenID == ASTERISK_OPERATOR) {
                 file << "MULT T" << vars_num << std::endl;
             }
-            check_children(node, index);
+            if (node->leaf1 != nullptr)
+                semantic_check(node->leaf1, index);
+            if (node->leaf2 != nullptr)
+                semantic_check(node->leaf2, index);
+            if (node->leaf3 != nullptr)
+                semantic_check(node->leaf3, index);
+            if (node->leaf4 != nullptr)
+                semantic_check(node->leaf4, index);
         }
-        else if (node->child_1 != nullptr){
-            
-            semantic_check(node->child_1, index);
-            
+        else if (node->leaf1 != nullptr){
+            semantic_check(node->leaf1, index);    
+              
+    }
+
+    else if (node->label == "<M>"){
+        if (node->token1.tokenID == ASTERISK_OPERATOR){
+            if (node->leaf1 != nullptr)
+                semantic_check(node->leaf1, index);
+            if (node->leaf2 != nullptr)
+                semantic_check(node->leaf2, index);
+            if (node->leaf3 != nullptr)
+                semantic_check(node->leaf3, index);
+            if (node->leaf4 != nullptr)
+                semantic_check(node->leaf4, index);
+                file << "MULT -1" << std::endl;
         }
-            
+        else if (node->leaf1 != nullptr)
+            semantic_check(node->leaf1, index);
     }
 
 
-    else if (node->name == "<M>"){
-        if (node->token_1.token_ID == ASTERISK_TK){
-            check_children(node, index);
-            file << "MULT -1" << std::endl;
-        }
-        else if (node->child_1 != nullptr)
-            semantic_check(node->child_1, index);
-    }
-
-
-    else if (node->name == "<A>"){
-        if (node->token_1.token_ID == PLUS_TK){
-            int vars_num = current_temp_vars_num++;
+    else if (node->label == "<A>"){
+        if (node->token1.tokenID == ADD_OPERATOR){
+            
+            if (node->leaf1 != nullptr)
+                semantic_check(node->leaf1, index);
+            int vars_num = t_var_index++;
+            
+            file << "STORE T" << vars_num << std::endl;
             file << "ADD T" << vars_num << std::endl;
-            check_children(node, index);
-
+            
+            if (node->leaf2 != nullptr)
+                semantic_check(node->leaf2, index);
+            if (node->leaf3 != nullptr)
+                semantic_check(node->leaf3, index);
+            if (node->leaf4 != nullptr)
+                semantic_check(node->leaf4, index);
         }
-        else if (node->child_1 != nullptr)
-            semantic_check(node->child_1, index);
+        else if (node->leaf1 != nullptr)
+            semantic_check(node->leaf1, index);
     }
 
 
-    else if (node->name == "<R>"){
-        if (node->token_1.token_ID == ID_TK){
-            if (!var_exists(node->token_1)){
-                error_declared(node->token_1.token_string);
+    else if (node->label == "<R>"){
+        if (node->leaf1 != nullptr) {
+        semantic_check(node->leaf1, index);
+        }
+        
+        else if (node->token1.tokenID == IDENTIFIER){
+            if (!var_exists(node->token1)){
+                error_declared(node->token1.token_string);
                 exit(EXIT_FAILURE);
             }
-            file << "LOAD " << node->token_1.token_string << std::endl;
+            file << "LOAD " << node->token1.token_string << std::endl;
         }
-        else if (node->child_1 != nullptr) {
-            
-            semantic_check(node->child_1, index);
+        else if (node->token1.tokenID == INTEGER) {
+            file << "LOAD " << node->token1.token_string << std::endl;
         }
     }
 
-    else if (node->name == "<out>")
+    else if (node->label == "<out>")
     {
-        if (node->child_1 != nullptr)
-            semantic_check(node->child_1, index);
-        std::string temp_var = get_temp_var();
+        if (node->leaf1 != nullptr)
+            semantic_check(node->leaf1, index);
+        std::string temp_var = make_t_var();
         file << "STORE " << temp_var << "\n";
         file << "WRITE " << temp_var << "\n";
     }
 
-    else if (node->name == "<in>"){
-        if (!var_exists(node->token_2)){
-            error_declared(node->token_1.token_string);
+    else if (node->label == "<in>"){
+        if (!var_exists(node->token2)){
+            error_declared(node->token1.token_string);
             exit(EXIT_FAILURE);
         }
         else {
-            int var_location = var_exists(node->token_1);
+            int var_location = var_exists(node->token1);
             if (var_location == -1)
             {
-                std::cout << "ERROR: " << node->token_1.token_string << " has not been declared in this scope" << std::endl;
+                std::cout << "ERROR: " << node->token1.token_string << " has not been declared in this scope" << std::endl;
                 exit(EXIT_FAILURE);
             }
 
-            std::string temp_var = get_temp_var();
+            std::string temp_var = make_t_var();
             file << "\t\tREAD " << temp_var << "\n";
             file << "\t\tLOAD " << temp_var << "\n";
             file << "\t\tSTACKW " << var_location << "\n";
         }
     }
 
-    else if (node->name == "<loop>") {
-        int loop_num = current_labels_num++;
-        int vars_num = current_temp_vars_num++;
+    else if (node->label == "<loop>") {
+        int loop_num = loop_index++;
+        int vars_num = t_var_index++;
 
         file << "L" << loop_num << ": NOOP" << std::endl;
-        semantic_check(node->child_1, index);
+        semantic_check(node->leaf1, index);
         file << "STORE T" << vars_num << std::endl;
-        semantic_check(node->child_3, index);
+        semantic_check(node->leaf3, index);
         file << "SUB T" << vars_num << std::endl;
 
-        int exit_loop_num = current_labels_num;
-        carry_labels_num = current_labels_num++;
-        semantic_check(node->child_2, index);
-        semantic_check(node->child_4, index);
+        int exit_loop_num = loop_index;
+        
+        semantic_check(node->leaf2, index);
+        semantic_check(node->leaf4, index);
         file << "BR L" << loop_num << std::endl;
         file << "L" << exit_loop_num << ": NOOP" << std::endl;
     }
 
 
-    else if (node->name == "<assign>"){
-        if (!var_exists(node->token_2)){
-            error_declared(node->token_2.token_string);
+    else if (node->label == "<assign>"){
+        if (!var_exists(node->token2)){
+            error_declared(node->token2.token_string);
             exit(EXIT_FAILURE);
         }
-        if (node->child_1 != nullptr) {
-            file << "STORE " << node->token_2.token_string << std::endl;
-            semantic_check(node->child_1, index);
+        if (node->leaf1 != nullptr) {
+            file << "STORE " << node->token2.token_string << std::endl;
+            semantic_check(node->leaf1, index);
 
         }
     }
     else {
-    check_children(node, index);
+            if (node->leaf1 != nullptr)
+                semantic_check(node->leaf1, index);
+            if (node->leaf2 != nullptr)
+                semantic_check(node->leaf2, index);
+            if (node->leaf3 != nullptr)
+                semantic_check(node->leaf3, index);
+            if (node->leaf4 != nullptr)
+                semantic_check(node->leaf4, index);
     }
 }
 
 // continues semantic check through children
-void check_children(Node* node, int index)
+void check_children(treeNode* node, int index)
 {
-    if (node->child_1 != nullptr)
-        semantic_check(node->child_1, index);
-    if (node->child_2 != nullptr)
-        semantic_check(node->child_2, index);
-    if (node->child_3 != nullptr)
-        semantic_check(node->child_3, index);
-    if (node->child_4 != nullptr)
-        semantic_check(node->child_4, index);
+    if (node->leaf1 != nullptr)
+        semantic_check(node->leaf1, index);
+    if (node->leaf2 != nullptr)
+        semantic_check(node->leaf2, index);
+    if (node->leaf3 != nullptr)
+        semantic_check(node->leaf3, index);
+    if (node->leaf4 != nullptr)
+        semantic_check(node->leaf4, index);
 }
 
 // error for declared variables
@@ -311,14 +361,14 @@ void show_stack() {
     std::cout << std::endl;
 }
 
-std::string get_temp_var()
+std::string make_t_var()
 {
     /* create the temp_var name */
-    std::string temp_var = "T" + std::to_string(temp_var_count + 1);
+    std::string temp_var = "T" + std::to_string(var_count_t + 1);
     /* put the temp_var in the temp_var array */
-    temp_vars[temp_var_count] = temp_var;
+    temp_vars[var_count_t] = temp_var;
     /* created new temp_var so increment the count of the variables */
-    temp_var_count++;
+    var_count_t++;
     /* return the temp_var name */
     return temp_var;
 }
